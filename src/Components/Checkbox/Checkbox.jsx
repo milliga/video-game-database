effa01ec55ee754e6f4bc5a9c56a89b7568e9aa7
codeaustin <fluxsor@gmail.com>
2022-09-25 14:49:31 -0400

    Removed tags and genres copies from Checkbox component as they were for testing and no longer needed.

--- src/Components/Checkbox/Checkbox.jsx
+++ src/Components/Checkbox/Checkbox.jsx
@@ -38,25 +38,20 @@ export const Checkbox = ({ id, name, slug, isTag }) => {
     const handleChange = (e) => {
         setIsChecked(!isChecked);
         if(isTag) {
-            let tagsCopy = [...tags];
             const tagStrings = tags.map((tag) => tag.name).toString();
             if(!tagStrings.includes(e.target.value)) {
                 const newTag = {id: id, name: e.target.value};
-                tagsCopy = [...tags, newTag];
                 setTags([...tags, newTag]);
             }
             else if (tagStrings.includes(e.target.value)) {
                 setTags(tags.filter((t) => t.name !== e.target.value));
-                tagsCopy = tags.filter((t) => t.name !== e.target.value)
             }
         
         }
         else {
-            let genresCopy = [...genres];
             const genreStrings = genres.map((genre) => genre.name).toString();
             if(!genreStrings.includes(e.target.value)) {
                 const newGenre = {id: id, name: e.target.value};
-                genresCopy = [...genres, newGenre];
                 setGenres([...genres, newGenre]);
             }
             else if (genreStrings.includes(e.target.value)) {
